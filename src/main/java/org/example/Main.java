4413c419210182aba5fe2dfc5636fa8ac760f035
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-04 12:32:32 +0100

    Updated code that updated first_name in actor_id 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,15 +11,20 @@ public class Main {
 
         Connection conn = DriverManager.getConnection(url, user, password);
 
-        String sqlRead = "Select * from Actor";
+//        String sqlRead = "Select * from Actor";
+//
+//        try (PreparedStatement statement = conn.prepareStatement(sqlRead)) {
+//            ResultSet result = statement.executeQuery();
+//            while (result.next()) {
+//                String name = result.getString("first_name");
+//                System.out.println(name);
+//            }
+//        }
 
-        try (PreparedStatement statement = conn.prepareStatement(sqlRead)) {
-            ResultSet result = statement.executeQuery();
-            while (result.next()) {
-                String name = result.getString("first_name");
-                System.out.println(name);
-            }
+        String sqlUpdate = "UPDATE Actor SET first_name = 'Brannz' WHERE actor_id = 1";
+        try (PreparedStatement statement = conn.prepareStatement(sqlUpdate)) {
+            int rowsUpdated = statement.executeUpdate();
+            System.out.println(rowsUpdated);
         }
-
     }
 }
