272f62a07de580121688b2d874c1b67e1c5cfe7e
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-04 12:12:35 +0100

    Added code to get all Actors from MySQL database

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,25 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         String url = "jdbc:mysql://localhost:3306/sakila";
         String user = "root";
         String password = "!Debbie1985@";
 
         Connection conn = DriverManager.getConnection(url, user, password);
+
+        String sqlRead = "Select * from Actor";
+
+        try (PreparedStatement statement = conn.prepareStatement(sqlRead)) {
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                String name = result.getString("first_name");
+                System.out.println(name);
+            }
+        }
 
     }
 }
